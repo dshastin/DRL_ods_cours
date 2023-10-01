afaef7b84799573ae911b67588fe2e726f2077d0
denis <denis.shastim@gmail.com>
2023-10-01 15:33:24 +0300

    smoothing bug fixed

--- hw1.py
+++ hw1.py
@@ -4,39 +4,24 @@ import time
 
 
 class CrossEntropyAgent:
-    def __init__(self, state_n, action_n, smoothing=None, l_value=None, q_value=0.6) -> None:
+    def __init__(self, state_n, action_n, smoothing=None, l_value=None) -> None:
         self.state_n = state_n
         self.action_n = action_n
         self.smoothing = smoothing
-        self.q_value = q_value
         self.l_value = l_value
         self.model = np.ones((self.state_n, self.action_n)) / self.action_n
 
     def get_action(self, state):
-        probs = self.model[state] / np.sum(self.model[state])
-        action = np.random.choice(np.arange(self.action_n), p=probs)
-        return action
-    
-    def _policy_smoothing(self, new_model):
-        for state in range(self.state_n):
-            if not np.sum(new_model[state]) == 0:
-                self.model[state] = new_model[state] * self.l_value + self.model[state] * (1 - self.l_value) 
-    
-    def _laplace_smoothing(self, new_model):
-        for state in range(self.state_n):
-            if not np.sum(new_model[state]) == 0:
-                self.model[state] = new_model[state] + self.l_value
+        probs = self.model[state]
+        try:
+            action = np.random.choice(np.arange(self.action_n), p=probs)
+            return action
+        except:
+            print(np.sum(probs))
 
-    def _no_smoothing(self, new_model):
-        self.model[np.sum(new_model, axis=1) != 0] = new_model[np.sum(new_model, axis=1) != 0]
-    
-    def _get_elite_trajectories(self, trajectories):
-        mean_rewards = [np.sum(trajectory['rewards']) for trajectory in trajectories]
-        quantile = np.quantile(mean_rewards, self.q_value)
-        elite_trajectories = [trajectory for trajectory in trajectories if np.sum(trajectory['rewards']) > quantile]
-        return elite_trajectories
 
-    def fit(self, trajectories):
+    
+    def fit(self, elite_trajectories):
         """Fit model
 
         Attrs:
@@ -45,17 +30,23 @@ class CrossEntropyAgent:
         """
         new_model = np.zeros((self.state_n, self.action_n))
 
-        elite_trajectories = self._get_elite_trajectories(trajectories)
         for trajectory in elite_trajectories:
             for state, action in zip(trajectory['states'], trajectory['actions']):
                 new_model[state][action] += 1
-        
-        if self.smoothing == 'policy':
-            self._policy_smoothing(new_model)
-        elif self.smoothing == 'laplace':
-            self._laplace_smoothing(new_model)
-        elif not self.smoothing:
-            self._no_smoothing(new_model)
+
+        for state in range(self.state_n):
+            if np.sum(new_model[state]) != 0:
+                if not self.smoothing:
+                    new_model[state] = new_model[state] / np.sum(new_model[state])
+                elif self.smoothing == 'laplace':
+                    new_model[state] = ((new_model[state] + self.l_value) / 
+                                        (np.sum(new_model[state]) + self.l_value * self.action_n))
+                elif self.smoothing == 'policy':
+                    new_model[state] = (self.model[state] * (1 - self.l_value) + 
+                                        (new_model[state] / np.sum(new_model[state])) * self.l_value)
+            else:
+                new_model[state] = self.model[state].copy()
+        self.model = new_model
 
 
 def get_trajectory(env, agent, max_steps, visualize=False):
@@ -73,7 +64,7 @@ def get_trajectory(env, agent, max_steps, visualize=False):
             break
         if visualize:
             env.render()
-            time.sleep(0.5)
+            time.sleep(0.1)
     return trajectory
 
 def get_stats(trajectories, display=True):
@@ -96,6 +87,13 @@ def get_stats(trajectories, display=True):
         print(f'\tmean reward: {np.mean(iteration_stats["rewards"])}')
     return iteration_stats
 
+def get_elite_trajectories(trajectories, q):
+        mean_rewards = [np.sum(trajectory['rewards']) for trajectory in trajectories]
+        quantile = np.quantile(mean_rewards, q)
+        elite_trajectories = [trajectory for trajectory in trajectories if np.sum(trajectory['rewards']) > quantile]
+        return elite_trajectories
+
+
 if __name__ == '__main__':
     env = gym.make('Taxi-v3')
     action_space = env.action_space.n
@@ -103,13 +101,15 @@ if __name__ == '__main__':
     n_iterations = 500
     n_trajectories = 1000
     n_steps = 200
+    smoothing_type = 'laplace'
     q = 0.7
-    l = 2
+    l = 1
 
     agent = CrossEntropyAgent(action_n=action_space, state_n=obesrv_space, 
-                              smoothing='laplace', l_value=l, q_value=q)
+                              smoothing=smoothing_type, l_value=l)
     
     start = time.time()
+
     for i in range(n_iterations):
         iteration_trajectories = []
         for n in range(n_trajectories):
@@ -119,5 +119,7 @@ if __name__ == '__main__':
             print(f'iteration: {i}/{n_iterations}')
             get_stats(iteration_trajectories, display=True)
 
-        agent.fit(iteration_trajectories)
+        elite_trajectories = get_elite_trajectories(iteration_trajectories, q)
+        agent.fit(elite_trajectories)
+
     print(f'total time: {time.time() - start}')
